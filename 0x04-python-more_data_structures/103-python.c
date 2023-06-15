08a8bc13a14019a190ceacfa9d2a135395ba86cb
Andrews0899 <andrewsmakafui350@gmail.com>
2023-06-15 05:58:50 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,70 +1,68 @@
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
 
 /**
- * print_python_bytes - Prints bytes information
- *
- * @p: Python Object
- * Return: no return
- */
-void print_python_bytes(PyObject *p)
-{
-	char *string;
-	long int size, i, limit;
-
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
-
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
-
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
-
-	if (size >= 10)
-		limit = 10;
-	else
-		limit = size + 1;
-
-	printf("  first %ld bytes:", limit);
-
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
-		else
-			printf(" %02x", 256 + string[i]);
-
-	printf("\n");
-}
-
-/**
- * print_python_list - Prints list information
- *
- * @p: Python Object
- * Return: no return
+ * print_python_list - Prints information about a Python list object
+ * @p: Pointer to the PyObject representing the list
  */
 void print_python_list(PyObject *p)
 {
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
+	Py_ssize_t size;
+	Py_ssize_t i;
 
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
+	if (!PyList_Check(p))
+	{
+		printf("Invalid Python list object\n");
+		return;
+	}
 
+	size = PyList_Size(p);
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Size of the list = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
+		printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 	}
+}
+
+/**
+ * print_python_bytes - Prints information about a Python bytes object
+ * @p: Pointer to the PyObject representing the bytes object
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size;
+	Py_ssize_t i;
+	char *bytes_str;
+
+	if (!PyBytes_Check(p))
+	{
+		printf("Invalid Python bytes object\n");
+		return;
+	}
+
+	size = PyBytes_Size(p);
+	printf("[.] bytes object info\n");
+	printf("  Size: %ld\n", size);
+	printf("  Trying string: %s\n", PyBytes_AsString(p));
+
+	if (size > 10)
+	{
+		size = 10;
+	}
+
+	bytes_str = PyBytes_AsString(p);
+	printf("  first %ld bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes_str[i]);
+		if (i != size - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
 
