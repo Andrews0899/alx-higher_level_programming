df85915da93c060004a1146e7250401b541ad4ea
Andrews0899 <andrewsmakafui350@gmail.com>
2023-09-16 10:26:50 -0700

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-A script that  takes in the name of a state
+This script  takes in the name of a state
 as an argument and lists all cities of that
 state, using the database `hbtn_0e_4_usa`.
 """
 
-import MySQLdb
-import sys
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
     """
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     from the database.
     """
 
-    db_connect = MySQLdb.connect(host="localhost", port=3306,
+    db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
     with db_connect.cursor() as db_cursor:
