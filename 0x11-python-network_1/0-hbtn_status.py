122ef41f009c3b22fb11fa43c97dfbed89e70309
Andrews0899 <andrewsmakafui350@gmail.com>
2023-09-29 19:06:47 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,15 @@
 """
 A Python script that fetches https://intranet.hbtn.io/status
 """
+
 import urllib.request
 
+
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         req = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(req)))
-    print('\t- content: {}'.format(req))
-    print('\t- utf8 content: {}'.format(req.decode("UTF-8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(req)))
+        print("\t- content: {}".format(req))
+        print("\t- utf8 content: {}".format(req.decode("utf-8")))
